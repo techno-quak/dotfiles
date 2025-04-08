15b135ab94db890380b174f73f9736c2735b3416
Quak <196471366+techno-quak@users.noreply.github.com>
2025-04-08 21:15:06 +0200

    Initial

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,3 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-end
+source ~/.config/fish/user_variables.fish
+source ~/.config/fish/abbreviations.fish
+set -x SSH_AUTH_SOCK "$XDG_RUNTIME_DIR/ssh-agent.socket"
